1fff3ca1eb43e2ba9d465c18554466519bfbf7a8
Claudio Benghi <claudio.benghi@gmail.com>
2021-04-14 17:46:59 +0200

    Compiles

---
+++ TryOutClass.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+
+namespace TryOut
+{
+	public partial class TryOutClass : ObservableObject
+	{
+		//[ObservableProperty]
+		//private string someValue;
+
+		//[ICommand]
+		//private void Funzia()
+		//{
+		//	SomeValue = "newValue";
+		//}
+	}
+}
