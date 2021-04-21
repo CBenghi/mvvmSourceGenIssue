6a2d2a2e553b34705536dea21fc53d1f914081ec
Claudio Benghi <claudio.benghi@gmail.com>
2021-04-21 18:18:03 +0200

    Project compiles and works with bindings.

--- TryOutClass.cs
+++ TryOutClass.cs
@@ -8,12 +8,22 @@ namespace TryOut
 	{
 		private int clickCount;
 
-		[ObservableProperty]
+		// the ObservableProperty attribute should trigger the generation of the SomeValue property
+		// (which is succesfully binded by the XAML in MainWindow)
+		[ObservableProperty] 
 		private string someValue = "Hi There";
 
+		// the ICommand attribute creates a FunziaCommand at compile time that is
+		// used in binding from the XAML in MainWindow
 		[ICommand]
 		private void Funzia()
 		{
+			// PROBLEM: The SomeValue property is generated programmatically and it is succesfully bound in XML
+			// but it cannot be referenced here, or the compilation will fail.
+			// This failure does happen if the project does not set <UseWPF>true</UseWPF>
+
+			// SomeValue = ""; 
+
 			if (SetProperty(ref someValue, clickCount % 2 == 0 ? "Have a great day" : "Hi There", "SomeValue"))
             {
 				clickCount++;
